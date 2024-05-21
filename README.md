88b14f24376b97725930061a5ae44bbf73e69aa5
raphavtorres <raphaeltorresprofissional@gmail.com>
2024-05-21 16:49:09 -0300

    chore: update gitignore and hardhat.config

--- README.md
+++ README.md
@@ -7,5 +7,11 @@ https://dev.to/oliverjumpertz/how-to-set-up-a-solidity-project-and-create-your-f
 https://docs.metamask.io/
 
 
-## course
+## Course
 https://www.freecodecamp.org/news/web3-curriculum-open-beta/
+
+## Sepolia
+https://ethereum.org/en/developers/docs/networks/#sepolia
+
+## Sepolia faucet
+https://faucet.quicknode.com/drip
--- web3/hardhat.config.js
+++ web3/hardhat.config.js
@@ -3,5 +3,21 @@ require("solidity-coverage");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-	solidity: "0.8.24",
+	solidity: {
+		version: "0.8.24",
+		defaultNetwork: "sepolia",
+		networks: {
+			hardhat: {},
+			sepolia: {
+				url: "https://sepolia.drpc.org",
+				accounts: [`0x${process.env.PRIVATE_KEY}`],
+			},
+		},
+		settings: {
+			optimizer: {
+				enabled: true,
+				runs: 200,
+			},
+		},
+	},
 };
